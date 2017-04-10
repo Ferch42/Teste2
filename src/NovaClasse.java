66feff8269ce63738bf9950228be0e88c31651bd
Ferch42 <fernando.hsieh@usp.br>
2017-04-10 18:25:04 -0300

    agsdhgj

--- src/NovaClasse.java
+++ src/NovaClasse.java
@@ -2,7 +2,7 @@
 public class abacazi {
 	public static void main(String[] args) {
 	
-		System.out.println("Finalmente consegui, posso ir comer?");
+		System.out.println("Finalmente consegui, posso ir jantar?");
 		System.out.println("não pode");
 		System.out.println("Come enquanto joga!");
 	}
