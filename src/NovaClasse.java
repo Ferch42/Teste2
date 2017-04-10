843d172a98e26bb02876b5051ff92facdd5a5b6a
Ferch42 <fernando.hsieh@usp.br>
2017-04-10 20:27:03 -0300

    Revert "oi"
    This reverts commit 57c7d172e56816585e6f84debfbccbc86f647155.

--- src/NovaClasse.java
+++ src/NovaClasse.java
@@ -2,7 +2,7 @@
 public class abacazi {
 	public static void main(String[] args) {
 	
-		System.out.println("Finalmente consegui, posso ir jantar as comidas?");
+		System.out.println("Finalmente consegui, posso ir jantar?");
 		System.out.println("não pode");
 		System.out.println("Come enquanto joga!");
 		System.out.println("Claro!!!");
