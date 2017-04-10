c07ffc4f15dc0f59030b703dec6034097889b71a
Harumi Yamamoto <ligia.yamamoto@usp.br>
2017-04-10 18:14:37 -0300

    OLAAA

--- src/abacazi.java
+++ src/abacazi.java
@@ -3,6 +3,7 @@ public class abacazi {
 	public static void main(String[] args) {
 		String h;
 		String g;
+		String i;
 		System.out.println("Deu certo!");
 		System.out.println("Não deu certo!!");
 	}
