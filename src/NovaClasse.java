9080cffa5cd694cb5592cd26a26c5a346a9f4974
Ferch42 <fernando.hsieh@usp.br>
2017-04-10 20:25:33 -0300

    Revert "OLA 5"
    This reverts commit 1461cbb23ae7795e7f01951815a9df9923fde3e5.

--- src/NovaClasse.java
+++ src/NovaClasse.java
@@ -6,6 +6,5 @@ public class abacazi {
 		System.out.println("não pode");
 		System.out.println("Come enquanto joga!");
 		System.out.println("Claro!!!");
-		System.out.println("Joga enquanto come!");
 	}
 }	
