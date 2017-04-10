1461cbb23ae7795e7f01951815a9df9923fde3e5
Harumi Yamamoto <ligia.yamamoto@usp.br>
2017-04-10 18:40:22 -0300

    OLA 5

--- src/NovaClasse.java
+++ src/NovaClasse.java
@@ -6,5 +6,6 @@ public class abacazi {
 		System.out.println("não pode");
 		System.out.println("Come enquanto joga!");
 		System.out.println("Claro!!!");
+		System.out.println("Joga enquanto come!");
 	}
 }	
